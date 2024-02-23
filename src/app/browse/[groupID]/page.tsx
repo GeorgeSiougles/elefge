394b001a00c6687b725e962d3973b028e51c1797
George Siougles <51824170+JojosS@users.noreply.github.com>
2024-02-23 14:17:38 +0200

    Fetch created listing

--- src/app/browse/[groupID]/page.tsx
+++ src/app/browse/[groupID]/page.tsx
@@ -1,10 +1,22 @@
+import MapListing from "@/models/MapListing";
+
 interface PageProps {
   params: {
     groupId: string;
   };
 }
 
-const Page = ({ params }: PageProps) => {
-  return <div>TODO chatting interface</div>;
+const Page = async ({ params }: PageProps) => {
+  const listing = await MapListing.findOne({
+    mapListingId: params.groupId,
+  });
+  console.log(listing);
+  return (
+    <div>
+      TODO chatting interface
+      <h1>Map Name: {listing.mapName}</h1>
+      <h1>GroupID:{params.groupId}</h1>
+    </div>
+  );
 };
 export default Page;
