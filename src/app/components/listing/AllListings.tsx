97d9abed28519aa96803218382d0789cba1a4581
George Siougles <51824170+JojosS@users.noreply.github.com>
2024-03-07 15:06:52 +0200

    Navigate to listing from browsing

--- src/app/components/listing/AllListings.tsx
+++ src/app/components/listing/AllListings.tsx
@@ -1,5 +1,6 @@
 import MapListing, { MapListingType } from "@/models/MapListing";
 import Listing from "./Listing";
+import Link from "next/link";
 
 const AllListings = async () => {
   const listings = await MapListing.find();
@@ -14,7 +15,9 @@ const AllListings = async () => {
           <div className="flex-1">Descrpition</div>
         </div>
         {listings.map((cur: MapListingType, index) => (
-          <Listing key={index} listing={cur} />
+          <Link key={index} href={`/browse/${cur.mapListingId}`}>
+            <Listing listing={cur} />
+          </Link>
         ))}
       </div>
     </div>
