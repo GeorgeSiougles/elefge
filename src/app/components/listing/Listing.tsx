6c76e5d1f51e4b3bfdffbecad3e574b38d3ede29
George Siougles <51824170+JojosS@users.noreply.github.com>
2024-03-07 14:40:57 +0200

    Fix owner data type

--- src/app/components/listing/Listing.tsx
+++ src/app/components/listing/Listing.tsx
@@ -8,7 +8,11 @@ const Listing = ({ listing }: ListingProps) => {
     <div className="flex justify-between bg-gray-400 hover:bg-white hover:text-black">
       <div className="flex-1">{listing.activityName}</div>
       <div className="flex-1">{listing.mapName}</div>
-      <div className="flex-1">{listing.owner[0].username}</div>
+      <div className="flex-1">
+        {Array.isArray(listing.owner)
+          ? listing.owner[0].email
+          : listing.owner.email}
+      </div>
       <div className="flex-1">{listing.description}</div>
     </div>
   );
